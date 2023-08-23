a54cf98fa69b6ce1ccdba8d0df37b22d2266f4d0
Ahmed <ahmed.hamed011127@gmail.com>
2023-08-23 07:40:58 -0700

    a

--- simple_printers.c
+++ simple_printers.c
@@ -59,7 +59,7 @@ int print_rot13(va_list ap, params_t *params)
 {
 	int i, index;
 	int count = 0;
-	char arr[] = 
+	char arr[] =
 		"NOPQRSTUVWXYZABCDEFGHIJKLM	nopqrstuvwxyzabcdefghijklm";
 	char *a = va_arg(ap, char *);
 	(void)params;
